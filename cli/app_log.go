e3469859b646a312486e12470ac749c6d69ffad2
lucas-code42 <lucasdearaujo.brandao@gmail.com>
2023-01-02 21:15:10 -0300

    update

--- cli/app_log.go
+++ cli/app_log.go
@@ -10,8 +10,8 @@ func Greet() {
 // ShowMenu mostra o menu
 func ShowMenu() {
 	fmt.Println("Menu:")
-	fmt.Println("1 - Mostrar todas as ações disponiveis")
+	fmt.Println("1 - Mostrar todas as ações disponiveis.")
 	fmt.Println("2 - Cotação dólar x real.")
-	fmt.Println("3 - TOP 100 ações mais baratas")
-	fmt.Println("4 - Ordenar por setor")
+	fmt.Println("3 - TOP 100 ações mais baratas.")
+	fmt.Println("4 - Ordenar por setor.")
 }
